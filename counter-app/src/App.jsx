05eb99c34bc13fe9c7aea21002ac47bddb957313
Mohammed Sameer Ahmed <sameerahmedmohammed4@gmail.com>
2026-08-24 21:17:23 +0530

    Added styling and reset button to the app

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -1,11 +1,12 @@
-import Counter from "./components/Counter";
+import React from 'react'
+
+import Counter from './components/Counter'
 
 function App() {
   return (
     <div>
-      <Counter />
+      <Counter/>
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
--- counter-app/src/components/Counter.jsx
+++ counter-app/src/components/Counter.jsx
@@ -1,32 +1,33 @@
-import { useState } from "react";
-import "./Counter.css";
+import React from 'react'
+import { useState } from 'react'
 
 function Counter() {
-  const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
+    const [prevCount, setprevCount] = useState(0);
 
-  const increase = () => {
-    setCount(count + 1);
-  };
-
-  const decrease = () => {
-    setCount(count - 1);
-  };
-
+    const increment = () => {
+        setprevCount(count)
+        setCount(count + 1);
+    } 
+    const decrement = () => {
+        setprevCount(count)
+        setCount(count - 1);
+    } 
+    const reset = () => {
+        setprevCount(count)
+        setCount(0);
+    }
+    
   return (
-    <div className="counter-container">
-      <h1>Counter</h1>
-
-      <h2 className="counter-value">{count}</h2>
-
-      <button className="counter-button" onClick={decrease}>
-        -
-      </button>
-
-      <button className="counter-button" onClick={increase}>
-        +
-      </button>
+    <div>
+          <h1>React Counter App</h1>
+          <div className="container">{count}</div>
+          <button id='btn1' onClick={decrement}>-</button>
+          <button id='btn2' onClick={reset}>Reset</button>
+          <button id='btn3' onClick={increment}>+</button>
+          <p >Counter changed from {prevCount} to {count}</p>
     </div>
-  );
+  )
 }
 
 export default Counter;
