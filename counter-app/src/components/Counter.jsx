77ecdb4b4c0babdff1989f7eadeb6003396d003e
Mohammed Sameer Ahmed <sameerahmedmohammed4@gmail.com>
2026-08-24 14:22:09 +0530

    Added simple styling to the code.

--- counter-app/src/components/Counter.jsx
+++ counter-app/src/components/Counter.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react"; 
+import { useState } from "react";
+import "./Counter.css";
 
-function Counter(){
-  const [count , setCount] = useState(0);
+function Counter() {
+  const [count, setCount] = useState(0);
 
   const increase = () => {
     setCount(count + 1);
@@ -12,13 +13,20 @@ function Counter(){
   };
 
   return (
-    <div>
-    <h1>Counter </h1>
-    <h2>{count}</h2>
-    <button onClick={decrease}> - </button>
-    <button onClick={increase}> + </button>
+    <div className="counter-container">
+      <h1>Counter</h1>
 
+      <h2 className="counter-value">{count}</h2>
+
+      <button className="counter-button" onClick={decrease}>
+        -
+      </button>
+
+      <button className="counter-button" onClick={increase}>
+        +
+      </button>
     </div>
-  )
+  );
 }
+
 export default Counter;
